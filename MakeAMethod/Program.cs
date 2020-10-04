b5213bda07d715ba6b89166d71a9e953773d3a91
Tori <brobstv@mail.nmc.edu>
2020-10-04 14:32:10 -0400

    first commit

---
+++ MakeAMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MakeAMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            // test1
+
+            age = DisplayGetUserAge();
+        }
+
+        static int DisplayGetUserAge()
+        {
+            int age;
+            string userResponse;
+            bool validResponse;
+
+            do
+            {
+                validResponse = true;
+
+                Console.Write("Enter Age: ");
+                userResponse = Console.ReadLine();
+
+                if (!int.TryParse(userResponse, out age))
+                {
+                    Console.WriteLine("Please enter a valid age.");
+                    validResponse = false;
+                }
+
+            } while (!validResponse);
+
+            return age;
+        }
+    }
+}
